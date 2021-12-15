58849bb021e42e084c2adc0a93b4f4d0edea9538
DmytroShymkivDev <dmytro.shymkiv.dev@gmail.com>
2021-12-15 12:37:40 +0200

    make changes on pwa

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [todos, setTodos] = useState();
+  const [id, setId] = useState<number>(1);
+
+  const fetchTodos = (id: number) => {
+    fetch(`https://jsonplaceholder.typicode.com/todos/${id}`)
+      .then((response) => response.json())
+      .then((json) => {
+        setTodos(json);
+        console.log(json);
+      });
+  };
+
+  useEffect(() => {
+    fetchTodos(id);
+  }, [id]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={() => setId((prev) => prev + 1)}>NEXT</button>
+      {todos ? <pre>{JSON.stringify(todos, null, 2)}</pre> : <h1>EMPTY</h1>}
     </div>
   );
 }
